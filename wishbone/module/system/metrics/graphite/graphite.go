c4a60b97dc09b02f84f863d3dea63b0567c3e9c5
Jelle Smet <development@smetj.net>
2014-02-04 21:46:16 +0100

    Adding metric support

---
+++ wishbone/module/system/metrics/graphite/graphite.go
@@ -0,0 +1,27 @@
+package graphite
+
+import "wishbone"
+import "wishbone/event"
+import "fmt"
+import "os"
+
+func NewModule(name string) actor.Actor {
+	graphite := actor.NewActor()
+	graphite.SetName(name)
+	graphite.CreateQueue("inbox", 0)
+	graphite.CreateQueue("outbox", 0)
+
+	c := generateConsumer(graphite)
+	graphite.RegisterConsumer(c, "inbox")
+
+	return graphite
+}
+
+func generateConsumer(a actor.Actor) func(event.Event) {
+
+	return func(e event.Event) {
+		metric := fmt.Sprintf("%v.%v.%v %v %v\n", e.Data.(actor.Metric).Source, os.Args[0], e.Data.(actor.Metric).Name, e.Data.(actor.Metric).Value, e.Data.(actor.Metric).Time)
+		e.Data = metric
+		a.Queuepool["outbox"].Queue <- e
+	}
+}
